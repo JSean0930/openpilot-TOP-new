b10b662150b0a45e4baa0c919e8aaeecdd92899a
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2025-02-20 19:39:29 +0800

    更新 interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -46,7 +46,7 @@ class CarInterface(CarInterfaceBase):
       ret.safetyConfigs[0].safetyParam |= Panda.FLAG_TOYOTA_LTA
 
       # LTA control can be more delayed and winds up more often
-      ret.steerActuatorDelay = 0.18
+      ret.steerActuatorDelay = 0.16
       ret.steerLimitTimer = 0.8
     else:
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
